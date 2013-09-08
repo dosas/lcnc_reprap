20acac06e7300b2ab98d384da2d4ae8bd80df379
dosas <philipp.heimpel@gmail.com>
2013-09-08 22:07:54 +0200

    postprocessing script edited

--- postprocessing.sh
+++ postprocessing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## postprocessing script for slic3r
+## replaces E with A, S with P, comments all M-codes but the M109 (set and wait temperature)
 
 sed -i 's/E/A/g;s/S/P/g;s/M[0-9][0-9]/;&/g;s/;\(M109\)/\1/g' $*  
 echo M2 >> $*
